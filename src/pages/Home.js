e0834d85eb2c83eb223de18a249c099c99aeee4b
Clifton Hill <clifton@cliftonh.com>
2021-01-09 12:55:54 -0800

    Adding User page functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 const axios = require("axios").default;
@@ -7,20 +8,26 @@ export default function Home() {
   // location of user Home where calendar is displayed, should log in directly to this page when going to root location if user token is still active.
     // I think Home and Splash would both be routed to within the App, once I understand React Router
 
-const [displayName, setDisplayName ] = useState("User Name");
+  const [displayName, setDisplayName ] = useState("User Name");
 
-axios.get("/api/user")
-  .then(response => {
-    setDisplayName(response.data.displayName)
-    console.log(response.data.displayName);
-    // need to find out why this logs 2-3 times, maybe need to try useEffect hook which supposedly can perform action only once.
-  })
-  .catch(err => console.log(err));
+  axios.get("/api/user")
+    .then(response => {
+      setDisplayName(response.data.displayName)
+      console.log(response.data.displayName);
+      // need to find out why this logs 2-3 times, maybe need to try useEffect hook which supposedly can perform action only once.
+    })
+    .catch(err => console.log(err));
+
+  const browserHistory = useHistory();
+
+  function handleClick () {
+    browserHistory.push("/user");
+  }
 
   return (
     <div>
     <Header />
-      <div>{ displayName } Calendar View</div>
+    <div><button onClick={ handleClick }>{ displayName }</button> Calendar View</div>
     <Footer />
     </div>
   );
